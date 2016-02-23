59f816e76063a342beed482b264b2d8819fdf8d7
leimiaomiao <leimiaomiao@126.com>
2016-02-23 12:12:06 +0400

    modify project controller.

--- application/controller/project_ctrl.py
+++ application/controller/project_ctrl.py
@@ -4,17 +4,9 @@ from application import app
 
 @app.route('/', methods=['GET'])
 def homepage():
-<<<<<<< HEAD
     return render_template("homepage3.html")
-=======
-    return render_template("homepage2.html")
->>>>>>> 4f416b02a5b308c783dbdf0513069ed80be0de23
 
 
 @app.route('/test', methods=['GET'])
 def test():
-<<<<<<< HEAD
     return render_template("homepage2.html")
-=======
-    return render_template("homepage.html")
->>>>>>> 4f416b02a5b308c783dbdf0513069ed80be0de23
